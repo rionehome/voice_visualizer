694b74aa73c1b9efedd65f8e1f543ac248be5749
nakano16180 <nakano16180@gmail.com>
2019-08-15 11:28:47 +0900

    [update] 更新時読み込みに対応

--- scripts/app.js
+++ scripts/app.js
@@ -12,23 +12,35 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname))
 
+var msg_history = [];
 rosnodejs.initNode('/visualizer')
     .then((rosNode) => {
       let sub = rosNode.subscribe('/sound_system/log/heard', std_msgs.String,
         (data) => {
           rosnodejs.log.info('I heard: [' + data.data + ']');
-          io.emit('other-message', data.data);  // 送信
+          var message_data = {
+            id: 'other-message',
+            msg_data: data.data
+          }
+          msg_history.push(message_data);
+          io.emit('message', message_data);  // 送信
         }
       );
       let sub2 = rosNode.subscribe('/sound_system/log/spoke', std_msgs.String,
         (data) => {
           rosnodejs.log.info('I spoke: [' + data.data + ']');
-          io.emit('message', data.data);  // 送信
+          var message_data = {
+            id: 'message',
+            msg_data: data.data
+          }
+          msg_history.push(message_data);
+          io.emit('message', message_data);  // 送信
         }
       );
 
       io.on('connection', (socket) => {
         console.log('connected');
+        io.emit('message_update', msg_history);
         socket.on('message', (msg) => {
           console.log("from input: "+msg);
           io.emit('message', msg);
